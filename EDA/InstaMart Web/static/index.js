012befbee465d7432b146c169c5cad8deb8141d8
Pratik_Umap <103456799+pratik-umap@users.noreply.github.com>
2023-02-10 18:45:19 +0530

    Add files via upload

---
+++ EDA/InstaMart Web/static/index.js
@@ -0,0 +1,10 @@
+// let getName=document.getElementById("getName");
+// let user=document.getElementById("userName")
+// let val;
+// getName.addEventListener('change',function () {
+//     val=this.value;
+   
+// })
+// user.innerText=val;
+// // let val=user.innerText="hi pratik";
+// // console.log(val)
